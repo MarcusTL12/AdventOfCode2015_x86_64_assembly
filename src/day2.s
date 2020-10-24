e6e445c6be3bffab05e0b0b363bfaae04dc9b6db
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-10-24 11:43:31 +0200

    _

--- src/day2.s
+++ src/day2.s
@@ -2,64 +2,94 @@
 .globl day2_part2
 .text
 
-day2_part1:
-    .set buffer_size, 10
+# Parameters
+# %rdi: pointer to string
+# %rsi: length of string
+# %rdx: %pointer to buffer to put the numbers
+split_numbers:
+    .set buffer_size, 3 * 8
+    
     push %r12
     push %r13
     
-    # Allocating local variables:
-    # %rbp - buffer_size = buffer of substring pointers
-    # %rbp - 2 * buffer_size = buffer of substring lengths
+    mov %rdx, %r13
+    
+    push %rbp
+    mov %rsp, %rbp
+    sub $buffer_size * 2, %rsp
+    
+    mov $'x', %rdx
+    lea -buffer_size(%rbp), %rcx
+    lea -buffer_size * 2(%rbp), %r8
+    mov $3, %r9
+    call str_split_char
+    
+    xor %r12, %r12
+    split_numbers_loop:
+        mov -buffer_size(%rbp, %r12, 8), %rdi
+        mov -buffer_size * 2(%rbp, %r12, 8), %rsi
+        call parse_str_int_n
+        
+        mov %rax, (%r13, %r12, 8)
+        
+        inc %r12
+        cmp $3, %r12
+        jl split_numbers_loop
+    
+    leave
+    
+    pop %r13
+    pop %r12
+    
+    ret
+
+day2_part1:
+    .set buffer_size, 3 * 8
+    
+    push %r12
+    
     push %rbp
     mov %rsp, %rbp
     sub $buffer_size, %rsp
-    sub $buffer_size, %rsp
     
-    mov %rbp, %r12
-    sub $buffer_size, %r12
-    mov %r12, %r13
-    sub $buffer_size, %r13
+    lea -buffer_size(%rbp), %r12
     
-    mov $test_string, %rdi
-    call print
-    call newline
-    
-    mov $test_string, %rdi
+    mov $test_nums, %rdi
     call strlen
+    
+    mov $test_nums, %rdi
     mov %rax, %rsi
-    mov $65, %rdx
+    mov %r12, %rdx
+    call split_numbers
     
-    mov %rbp, %rcx
-    sub $buffer_size, %rcx
-    mov %rcx, %r8
-    sub $buffer_size, %r8
-    
-    mov $buffer_size, %r9
-    call str_split_char
-    
-    mov %rax, %rdi
+    mov 0(%r12), %rdi
     call print_int_dec_s
     call newline
     
-    mov (%r13), %rdi
+    mov 8(%r12), %rdi
     call print_int_dec_s
     call newline
     
     mov 16(%r12), %rdi
-    mov 16(%r13), %rsi
-    call print_n
+    call print_int_dec_s
     call newline
     
+    
     leave
-    pop %r13
+    
     pop %r12
+    
     ret
 
 day2_part2:
+    
     ret
 
 
 .data
 
-test_string:
-    .string "ThisAisAsomeAtestAdata"
+test_nums:
+    .string "6x26x20"
+
+filename:
+    .string "inputfiles/day2.txt"
