eb3a3d399541bb5d1dd4bd5f8b51643248a57737
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-10-27 22:50:11 +0100

    Day2

--- src/day2.s
+++ src/day2.s
@@ -43,12 +43,37 @@ split_numbers:
     
     ret
 
-
+# Parameters
+# %rdi: pointer to array of length, width and height
+wrapping_paper:
+    # %r8 = a * b
+    mov 0(%rdi), %r8
+    imul 8(%rdi), %r8
+    
+    # %r9 = b * c
+    mov 8(%rdi), %r9
+    imul 16(%rdi), %r9
+    
+    # %r10 = c * a
+    mov 16(%rdi), %r10
+    imul 0(%rdi), %r10
+    
+    mov %r8, %rdi
+    mov %r9, %rsi
+    mov %r10, %rdx
+    call min_3
+    
+    add %r9, %r8
+    add %r10, %r8
+    shl $1, %r8
+    
+    add %r8, %rax
+    ret
 
 day2_part1:
     .set fr_size, 40
     .set num_buf_size, 3 * 8
-    .set file_buf_size, 1024
+    .set file_buf_size, 512
     .set line_buf_size, 16
     
     push %r12
@@ -74,6 +99,9 @@ day2_part1:
     # Store num buffer pointer in %r14
     lea -fr_size - num_buf_size(%rbp), %r14
     
+    # Store final answer in %r15
+    xor %r15, %r15
+    
     # Make file reader
     mov %r12, %rdi
     lea -fr_size - num_buf_size - file_buf_size(%rbp), %rdx
@@ -92,12 +120,17 @@ day2_part1:
         mov %r14, %rdx
         call split_numbers
         
+        mov %r14, %rdi
+        call wrapping_paper
         
-        
-        call newline
+        add %rax, %r15
         
         jmp part1_loop
     part1_loop_end:
+    
+    mov %r15, %rdi
+    call print_int_dec_s
+    call newline
     
     # Close file
     mov (%r12), %rdi
@@ -112,11 +145,46 @@ day2_part1:
     
     ret
 
+# Parameters:
+# %rdi: Array of length, width and height
+ribbon:
+    mov 0(%rdi), %r8
+    mov 8(%rdi), %r9
+    mov 16(%rdi), %r10
+    
+    # Bubble sort the three values
+    cmp %r8, %r9
+    cmovl %r8, %r11
+    cmovl %r9, %r8
+    cmovl %r11, %r9
+    
+    cmp %r9, %r10
+    cmovl %r9, %r11
+    cmovl %r10, %r9
+    cmovl %r11, %r10
+    
+    cmp %r8, %r9
+    cmovl %r8, %r11
+    cmovl %r9, %r8
+    cmovl %r11, %r9
+    
+    # Add the two lowes sides together and multiply by 2
+    mov %r8, %rax
+    add %r9, %rax
+    shl %rax
+    
+    # Multiply together the sides and add to ans
+    imul %r8, %r10
+    imul %r9, %r10
+    add %r10, %rax
+    
+    ret
+
 day2_part2:
     .set fr_size, 40
     .set num_buf_size, 3 * 8
-    .set file_buf_size, 17
-    .set line_buf_size, 4
+    .set file_buf_size, 512
+    .set line_buf_size, 16
     
     push %r12
     push %r13
@@ -141,6 +209,9 @@ day2_part2:
     # Store num buffer pointer in %r14
     lea -fr_size - num_buf_size(%rbp), %r14
     
+    # Store final answer in %r15
+    xor %r15, %r15
+    
     # Make file reader
     mov %r12, %rdi
     lea -fr_size - num_buf_size - file_buf_size(%rbp), %rdx
@@ -149,17 +220,27 @@ day2_part2:
     
     part2_loop:
         mov %r12, %rdi
-        call read_buf_file_byte
-        jz part2_loop_end
+        mov %r13, %rsi
+        call read_buf_file_line
+        test %rax, %rax
+        jz part1_loop_end
         
-        movb %al, (%r13)
         mov %r13, %rdi
-        mov $1, %rsi
-        call print_n
-        # call newline
+        mov %rax, %rsi
+        mov %r14, %rdx
+        call split_numbers
+        
+        mov %r14, %rdi
+        call ribbon
+        
+        add %rax, %r15
         
         jmp part2_loop
     part2_loop_end:
+    
+    mov %r15, %rdi
+    call print_int_dec_s
+    call newline
     
     # Close file
     mov (%r12), %rdi
@@ -181,8 +262,7 @@ test_nums:
     .string "6x26x20"
 
 filename:
-    .string "inputfiles/day2/ex1.txt"
-    # .string "testfile.txt"
+    .string "inputfiles/day2/input.txt"
 
 space:
     .string " "
