700d8718a33aa9b8172678308d5d175e096e96c5
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-11-16 21:59:58 +0100

    Day 7

--- src/day7.s
+++ src/day7.s
@@ -167,6 +167,43 @@ prog_map_iter_vals:
 
 # Parameters:
 # %rdi: program map
+# %rsi: pointer to string
+# %rdx: length of string
+# Returns 16 bit instruction result
+eval_int_or_wire:
+    push %rbx
+    push %r12
+    push %r13
+    mov %rdi, %rbx
+    mov %rsi, %r12
+    mov %rdx, %r13
+    
+    mov %r12, %rdi
+    mov %r13, %rsi
+    call str_is_number
+    
+    test %rax, %rax
+    jz eval_instruction_single_wire
+    
+    mov %r12, %rdi
+    mov %r13, %rsi
+    call parse_str_int_n
+    
+    jmp eval_int_or_wire_end
+    eval_instruction_single_wire:
+        mov %rbx, %rdi
+        mov %r12, %rsi
+        mov %r13, %rdx
+        call eval_wire
+    eval_int_or_wire_end:
+    pop %r13
+    pop %r12
+    pop %rbx
+    ret
+
+
+# Parameters:
+# %rdi: program map
 # %rsi: pointer to instruction string
 # %rdx: length of instruction string
 # Returns 16 bit instruction result
@@ -180,11 +217,86 @@ eval_instruction:
     push %rbx
     mov %rdi, %rbx
     
+    mov %rsi, %rdi
+    mov %rdx, %rsi
+    mov $' ', %rdx
+    lea -substr_ptrs(%rbp), %rcx
+    lea -substr_lens(%rbp), %r8
+    mov $3, %r9
+    call str_split_char
     
+    dec %rax
+    jz eval_instruction_single
+    dec %rax
+    jz eval_instruction_double
+    dec %rax
+    jz eval_instruction_tripple
+    mov $-1, %rax
+    jmp eval_instruction_end
     
+    eval_instruction_single:
+        mov %rbx, %rdi
+        mov -substr_ptrs(%rbp), %rsi
+        mov -substr_lens(%rbp), %rdx
+        call eval_int_or_wire
+        
+        jmp eval_instruction_end
+    eval_instruction_double:
+        mov %rbx, %rdi
+        mov -substr_ptrs + 8(%rbp), %rsi
+        mov -substr_lens + 8(%rbp), %rdx
+        call eval_int_or_wire
+        
+        not %ax
+        
+        jmp eval_instruction_end
+    eval_instruction_tripple:
+        push %r12
+        push %r13
+        
+        mov %rbx, %rdi
+        mov -substr_ptrs(%rbp), %rsi
+        mov -substr_lens(%rbp), %rdx
+        call eval_int_or_wire
+        mov %rax, %r12
+        
+        mov %rbx, %rdi
+        mov -substr_ptrs + 16(%rbp), %rsi
+        mov -substr_lens + 16(%rbp), %rdx
+        call eval_int_or_wire
+        mov %rax, %r13
+        
+        mov -substr_ptrs + 8(%rbp), %r8
+        xor %r9, %r9
+        mov (%r8), %r9b
+        
+        xor %r8, %r8
+        mov %r12w, %r8w
+        or %r13w, %r8w
+        cmp $'O', %r9b
+        cmove %r8, %rax
+        
+        mov %r12w, %r8w
+        and %r13w, %r8w
+        cmp $'A', %r9b
+        cmove %r8, %rax
+        
+        mov %r12w, %r8w
+        mov %r13b, %cl
+        shr %cl, %r8w
+        cmp $'R', %r9b
+        cmove %r8, %rax
+        
+        mov %r12w, %r8w
+        mov %r13b, %cl
+        shl %cl, %r8w
+        cmp $'L', %r9b
+        cmove %r8, %rax
+        
+        pop %r13
+        pop %r12
+    eval_instruction_end:
     pop %rbx
-    
-    mov $0, %ax
     leave
     ret
 
@@ -271,10 +383,6 @@ day7_part1:
     
     mov %rax, %rbx
     
-    # mov %rbx, %rdi
-    # mov $prog_map_iter, %rsi
-    # call hashmap_scan
-    
     mov %rbx, %rdi
     mov $target_wire, %rsi
     mov $1, %rdx
@@ -284,10 +392,6 @@ day7_part1:
     mov %ax, %di
     call print_int_dec_s
     call newline
-    
-    mov %rbx, %rdi
-    mov $prog_map_iter_vals, %rsi
-    call hashmap_scan
     
     mov %rbx, %rdi
     call hashmap_free
@@ -301,17 +405,104 @@ day7_part1:
     ret
 
 
+# Parameters:
+# %rdi: Pointer to struct
+# Returns true
+prog_map_iter_reset:
+    movb $0, 32(%rdi)
+    mov $1, %rax
+    ret
+
+
 day7_part2:
+    push %rbx
+    
+    .set input_buffer, 8
+    .set input_size, input_buffer + 8
+    .set key, input_size + 16
+    push %rbp
+    mov %rsp, %rbp
+    sub $key, %rsp
+    
+    mov $inputfile, %rdi
+    call file_size
+    mov %rax, -input_size(%rbp)
+    mov %rax, %rdi
+    call malloc
+    mov %rax, -input_buffer(%rbp)
+    
+    mov $inputfile, %rdi
+    call open_file_r
+    mov %rax, %rbx
+    
+    mov %rbx, %rdi
+    mov -input_buffer(%rbp), %rsi
+    mov -input_size(%rbp), %rdx
+    call read_file
+    
+    mov %rbx, %rdi
+    call close_file
+    
+    mov -input_buffer(%rbp), %rdi
+    mov -input_size(%rbp), %rsi
+    call parse_input
+    
+    mov %rax, %rbx
+    
+    mov %rbx, %rdi
+    mov $target_wire, %rsi
+    mov $1, %rdx
+    call eval_wire
+    
+    push %rax
+    
+    mov %rbx, %rdi
+    mov $prog_map_iter_reset, %rsi
+    call hashmap_scan
+    
+    movq $1, -key(%rbp)
+    movq $target_wire_2, -key + 8(%rbp)
+    mov %rbx, %rdi
+    lea -key(%rbp), %rsi
+    call hashmap_get
+    
+    pop %rcx
+    
+    movb $1, 32(%rax)
+    mov %cx, 33(%rax)
+    
+    mov %rbx, %rdi
+    mov $target_wire, %rsi
+    mov $1, %rdx
+    call eval_wire
+    
+    xor %rdi, %rdi
+    mov %ax, %di
+    call print_int_dec_s
+    call newline
+    
+    mov %rbx, %rdi
+    call hashmap_free
+    
+    mov -input_buffer(%rbp), %rdi
+    call free
+    
+    leave
+    
+    pop %rbx
     ret
 
 
 .data
 
 inputfile:
-    .string "inputfiles/day7/ex1.txt"
+    .string "inputfiles/day7/input.txt"
 
 target_wire:
-    .string "i"
+    .string "a"
+
+target_wire_2:
+    .string "b"
 
 arrow:
     .string " => "
